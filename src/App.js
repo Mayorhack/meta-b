68a0a9fca1eaed2bd7c15c1cc0f053bc5548f4b9
Mayowa Sodipo <sodipomayowa7@gmail.com>
2022-11-14 23:54:22 +0100

    fix:updated the app.js with the scroll to top component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./layout/Navbar";
-import Footer from "./layout/Footer";
+
 import Home from "./pages/Home";
 
 import Place from "./pages/Place";
+import Layout from "./layout/Layout";
+import ScrollToTop from "./layout/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/place" element={<Place />} />
-        </Routes>
-        <Footer />
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route exact path="" element={<Home />} />
+              <Route path="place" element={<Place />} />
+            </Route>
+          </Routes>
+        </ScrollToTop>
       </BrowserRouter>
     </div>
   );
