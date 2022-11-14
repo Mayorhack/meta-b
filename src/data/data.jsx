d87483da66737915abbc3b9c5d8d71029d9c75c8
Mayowa Sodipo <sodipomayowa7@gmail.com>
2022-11-14 23:45:19 +0100

    feat:added the images using data.jsx

---
+++ src/data/data.jsx
@@ -0,0 +1,45 @@
+import image from "../assets/Frame 151.png";
+import image2 from "../assets/Frame 152.png";
+import image3 from "../assets/Frame 153.png";
+import image4 from "../assets/Frame 154.png";
+import image5 from "../assets/Frame 155.png";
+import image6 from "../assets/Frame 156.png";
+import image7 from "../assets/Frame 157.png";
+import image8 from "../assets/Frame 158.png";
+import image9 from "../assets/Frame 159.png";
+import image10 from "../assets/Frame 1510.png";
+import image11 from "../assets/Frame 1511.png";
+import image12 from "../assets/Frame 1512.png";
+import image13 from "../assets/Frame 1513.png";
+import image14 from "../assets/Frame 1514.png";
+import image15 from "../assets/Frame 1515.png";
+import image16 from "../assets/Frame 1516.png";
+
+export const homeData = [
+  { image: image },
+  { image: image2 },
+  { image: image3 },
+  { image: image4 },
+  { image: image5 },
+  { image: image6 },
+  { image: image7 },
+  { image: image8 },
+];
+export const placeData = [
+  { image: image5 },
+  { image: image6 },
+  { image: image7 },
+  { image: image8 },
+  { image: image },
+  { image: image2 },
+  { image: image3 },
+  { image: image4 },
+  { image: image9 },
+  { image: image10 },
+  { image: image11 },
+  { image: image12 },
+  { image: image13 },
+  { image: image14 },
+  { image: image15 },
+  { image: image16 },
+];
