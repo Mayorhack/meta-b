f8fb21761bb1ccc2f63cc7179c7210276625e158
Mayowa Sodipo <sodipomayowa7@gmail.com>
2022-11-14 23:46:34 +0100

    refactor:added keys to the mapped fuunction

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ImageCard from "./ImageCard";
-import frame1 from "../assets/Frame1.png";
 
-const Collections = () => {
+const Collections = ({ data }) => {
   return (
     <div className="collections">
-      {[1, 2, 3, 4, 5, 6, 7, 8].map(() => {
-        return <ImageCard src={frame1} />;
+      {data.map((item, index) => {
+        return <ImageCard src={item.image} key={index} />;
       })}
     </div>
   );
